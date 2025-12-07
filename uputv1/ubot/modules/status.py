370e5678b0c73f5f7a1a1a7b392280b3fd219811
dep <depankenzo8@gmail.com>
2025-12-07 21:11:07 +0700

    Delete uputv1 directory

--- uputv1/ubot/modules/status.py
+++
@@ -1,9 +0,0 @@
-from ubot import *
-
-@PY.BOT("status")
-async def _(client, message):
-    await profile_command(client, message)
-
-@PY.CALLBACK("start_profile")
-async def _(client, message):
-    await ewdsfgj(client, message)
